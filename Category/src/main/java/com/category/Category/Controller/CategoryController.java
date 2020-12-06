c25cda2d9aae1b6c59c191f9f8bae3df0635d3e0
akaraman <akaraman@albarakaturk.com.tr>
2020-12-06 15:25:03 +0300

    Category service oluşturulması.

--- Category/src/main/java/com/category/Category/Controller/CategoryController.java
+++ Category/src/main/java/com/category/Category/Controller/CategoryController.java
@@ -1,12 +1,26 @@
 package com.category.Category.Controller;
+import com.category.Category.Entity.Category;
+import com.category.Category.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CategoryController {
+
+    @Autowired
+    private CategoryService _categoryService;
+
     @GetMapping("")
     public String getCategory() {
         return "Category List";
+    }
+
+    @PostMapping("/")
+    public Category saveCategory(@RequestBody Category category) {
+        return _categoryService.saveCategory(category);
     }
 }
---
+++ Category/src/main/java/com/category/Category/Service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.category.Category.Service;
+
+import com.category.Category.Entity.Category;
+import com.category.Category.Repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository _categoryRepository;
+
+    public Category saveCategory(Category category) {
+        return _categoryRepository.save(category);
+    }
+}
