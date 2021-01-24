d4909d0c6a2c1665bea2fbade0369279a866a4a8
akaraman <akaraman@albarakaturk.com.tr>
2021-01-24 13:28:11 +0300

    product servis config server baglantisi yapildi.

--- ProductService/src/main/java/com/product/ProductService/Controller/ProductController.java
+++ ProductService/src/main/java/com/product/ProductService/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.product.ProductService.Controller;
 import com.product.ProductService.Entity.Product;
 import com.product.ProductService.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,12 @@ public class ProductController {
     @Autowired
     private ProductService _productService;
 
+    @Value("${core.url}")
+    private String coreurl;
+
     @PostMapping("/")
     public Product saveProduct(@RequestBody Product product) {
-        
+
         return _productService.saveProduct(product);
     }
 }
